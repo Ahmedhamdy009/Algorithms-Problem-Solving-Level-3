3e426c23d82ec0ba5dc64146d9df9eb55ce79f06
Ahmedhamdy009 <ahmedhamdybayoumi2006@gmail.com>
2026-09-19 12:31:11 +0300

    update

--- Problem-02-Sum-Each-Row-In-Matrix/Solution-01.cpp
+++ Problem-02-Sum-Each-Row-In-Matrix/Solution-01.cpp
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-// Write your C++ solution here.
-// Write your C++ solution here.
-=======
+
 /*
  * Problem Statement:
  * Write a program to fill a 3x3 matrix with random numbers, then print the sum of each row.
  */
-
->>>>>>> c13fdc0b49b20e6ffb9a52c580efabc960d5869d
 #include <iostream>
 using namespace std;
 
