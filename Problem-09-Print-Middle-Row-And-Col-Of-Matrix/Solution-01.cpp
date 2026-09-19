2a06a8f46c230bac3d37c3a56f78f2d332656046
Ahmed Hamdy <ahmedhamdybayoumi2006@gmail.com>
2026-09-19 12:06:12 +0300

    Add problem statement to Problem-09-Print-Middle-Row-And-Col-Of-Matrix

--- Problem-09-Print-Middle-Row-And-Col-Of-Matrix/Solution-01.cpp
+++ Problem-09-Print-Middle-Row-And-Col-Of-Matrix/Solution-01.cpp
@@ -1,3 +1,8 @@
+/*
+ * Problem Statement:
+ * Write a program to fill a 3x3 matrix with random numbers, print it, then print the middle row and the middle column.
+ */
+
 #include <iostream>
 using namespace std;
 
