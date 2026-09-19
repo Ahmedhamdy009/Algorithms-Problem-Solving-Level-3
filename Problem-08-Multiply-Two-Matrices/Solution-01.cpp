8c02d28b7621ebdc21745716eee8753105dd20c8
Ahmed Hamdy <ahmedhamdybayoumi2006@gmail.com>
2026-09-19 12:05:48 +0300

    Add problem statement to Problem-08-Multiply-Two-Matrices

--- Problem-08-Multiply-Two-Matrices/Solution-01.cpp
+++ Problem-08-Multiply-Two-Matrices/Solution-01.cpp
@@ -1,3 +1,8 @@
+/*
+ * Problem Statement:
+ * Write a program to fill two 3x3 matrices with random numbers, then multiply them into a 3x3 matrix and print the result.
+ */
+
 #include <iostream>
 using namespace std;
 
