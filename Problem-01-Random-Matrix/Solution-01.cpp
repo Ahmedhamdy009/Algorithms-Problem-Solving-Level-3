eb6e7c8196963794c0c716de3813526c110b6ef0
Ahmedhamdy009 <ahmedhamdybayoumi2006@gmail.com>
2026-09-19 12:16:25 +0300

    update

--- Problem-01-Random-Matrix/Solution-01.cpp
+++ Problem-01-Random-Matrix/Solution-01.cpp
@@ -1,15 +1,44 @@
-<<<<<<< HEAD
-=======
-/*
- * Problem Statement:
- * 
- */
-
+// Write your C++ solution here.
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+
 using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << arr[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    return 0;
+    srand((unsigned)time(NULL));
+    int arr[3][3];
+    FillMatrixWithRandomNumbers(arr, 3, 3);
+    PrintMatrix(arr, 3, 3);
+
+    system("pause>0");
 }
->>>>>>> c13fdc0b49b20e6ffb9a52c580efabc960d5869d
