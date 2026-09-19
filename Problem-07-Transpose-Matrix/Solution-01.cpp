db51c078ace843349a722820f13e2d845f62e880
Ahmed Hamdy <ahmedhamdybayoumi2006@gmail.com>
2026-09-19 12:05:44 +0300

    Add problem statement to Problem-07-Transpose-Matrix

--- Problem-07-Transpose-Matrix/Solution-01.cpp
+++ Problem-07-Transpose-Matrix/Solution-01.cpp
@@ -1,3 +1,8 @@
+/*
+ * Problem Statement:
+ * Write a program to fill a 3x3 matrix with ordered numbers and print it, then transpose the matrix and print it.
+ */
+
 #include <iostream>
 using namespace std;
 
