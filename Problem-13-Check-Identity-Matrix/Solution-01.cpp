0322f9b800d472214a68c9eaca4e650244607523
Ahmed Hamdy <ahmedhamdybayoumi2006@gmail.com>
2026-09-19 12:06:32 +0300

    Add problem statement to Problem-13-Check-Identity-Matrix

--- Problem-13-Check-Identity-Matrix/Solution-01.cpp
+++ Problem-13-Check-Identity-Matrix/Solution-01.cpp
@@ -1,1 +1,8 @@
+/*
+ * Problem Statement:
+ * Write a program to check if the matrix is an identity matrix or not.
+ * 
+ * An identity matrix has `1` on its main diagonal and `0` in all other positions.
+ */
+
 // Write your C++ solution here.
