ec0eb8161b75f7fdb0996900004aeb418b93904a
Ahmed Hamdy <ahmedhamdybayoumi2006@gmail.com>
2026-09-19 12:06:36 +0300

    Add problem statement to Problem-14-Check-Scalar-Matrix

--- Problem-14-Check-Scalar-Matrix/Solution-01.cpp
+++ Problem-14-Check-Scalar-Matrix/Solution-01.cpp
@@ -1,1 +1,8 @@
+/*
+ * Problem Statement:
+ * Write a program to check if the matrix is a scalar matrix or not.
+ * 
+ * A scalar matrix has the same number on the main diagonal and `0` in all other positions.
+ */
+
 // Write your C++ solution here.
