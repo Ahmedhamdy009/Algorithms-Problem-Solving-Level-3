dcb66b26a351b45375b104d65d8b8388521233f0
Ahmedhamdy009 <ahmedhamdybayoumi2006@gmail.com>
2026-09-21 13:56:28 +0300

    update

--- Problem-07-Transpose-Matrix/Solution-01.cpp
+++ Problem-07-Transpose-Matrix/Solution-01.cpp
@@ -4,9 +4,61 @@
  */
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+
 using namespace std;
+
+void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols)
+{
+    int Counter = 0;
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            Counter++;
+            arr[i][j] = Counter;
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            cout << setw(3) << arr[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
+void TrasposeMatrix(int arr[3][3], int TrasposeArr[3][3], int Rows, int Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            TrasposeArr[i][j] = arr[j][i];
+        }
+    }
+}
 
 int main()
 {
-    return 0;
+
+    int arr[3][3];
+    int TrasposeArr[3][3];
+
+    FillMatrixWithOrderedNumbers(arr, 3, 3);
+
+    cout << "\nThe following is a 3x3 Ordered matrix:\n";
+    PrintMatrix(arr, 3, 3);
+
+    TrasposeMatrix(arr, TrasposeArr, 3, 3);
+
+    cout << "\nThe following is the traspose matrix:\n";
+    PrintMatrix(TrasposeArr, 3, 3);
+    system("pause>0");
 }
--- Problem-08-Multiply-Two-Matrices/Solution-01.cpp
+++ Problem-08-Multiply-Two-Matrices/Solution-01.cpp
@@ -4,9 +4,73 @@
  */
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+
 using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols)
+{
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+
+            arr[i][j] = RandomNumber(1, 10);
+        }
+    }
+}
+void MultipledMatrix(int Matrix1[3][3], int Matrix2[3][3], int MulitpledMatrix[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            MulitpledMatrix[i][j] = Matrix1[j][i] * Matrix2[i][j];
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            // cout << setw(3) << "   ";
+            printf("%0*d  ", 2, arr[i][j]);
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
-    return 0;
+    srand((unsigned)time(NULL));
+
+    int Matrix1[3][3], Matrix2[3][3];
+    int multipledMatrix[3][3];
+
+    FillMatrixWithOrderedNumbers(Matrix1, 3, 3);
+    FillMatrixWithOrderedNumbers(Matrix2, 3, 3);
+
+    cout << "\n Matrix1 \n";
+    PrintMatrix(Matrix1, 3, 3);
+
+    cout << "\n Matrix2 \n";
+    PrintMatrix(Matrix2, 3, 3);
+
+    MultipledMatrix(Matrix1, Matrix2, multipledMatrix, 3, 3);
+
+    cout << "\n Results : \n";
+    PrintMatrix(multipledMatrix, 3, 3);
+
+    system("pause>0");
 }
