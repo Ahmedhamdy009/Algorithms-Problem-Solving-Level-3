a21ca043a002ce70ca01cd73b185cb5ab7b72e83
Ahmedhamdy009 <ahmedhamdybayoumi2006@gmail.com>
2026-09-22 14:34:22 +0300

    update

--- Problem-09-Print-Middle-Row-And-Col-Of-Matrix/Solution-01.cpp
+++ Problem-09-Print-Middle-Row-And-Col-Of-Matrix/Solution-01.cpp
@@ -4,9 +4,120 @@
  */
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
+
 using namespace std;
+
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols)
+{
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+
+            arr[i][j] = RandomNumber(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            // cout << setw(3) << "   ";
+            printf("%0*d  ", 2, arr[i][j]);
+        }
+        cout << endl;
+    }
+}
+
+void PrintMiddleRowOfMatrix(int arr[3][3], short Row, short Cols)
+{
+    // cout << floor(3 % 2);//1
+    // int middleOfTheRow = 1;
+
+    short middleRow = Row / 2; // 3/ 2 = 1
+
+    // arr[1][0]    //arr[1][1]   //arr[1][2]
+    for (int j = 0; j < Cols; j++)
+    {
+        printf(" %0*d   ", 2, arr[middleRow][j]); // هنثبت ال Row ب 1
+    }
+}
+
+void PrintMiddleColOfMatrix(int arr[3][3], short Row, short Cols)
+{
+
+    short middleCol = Cols / 2; // 3% 2 = 1
+
+    for (int j = 0; j < Cols; j++)
+    {
+        printf(" %0*d   ", 2, arr[j][middleCol]); // هنثبت ال Col  ب 1
+    }
+}
+
+// او
+// void PrintMiddleRowOfMatrix(int arr[3][3], short Row, short Cols)
+// {
+//      cout << floor(3 % 2);//1
+
+//      int middleOfTheRow = 1;
+
+//     int middleOfTheRow = floor(Row % 2);//3% 2 = 1
+//     arr[1][0]    //arr[1][1]   //arr[1][2]
+//     for (int i = middleOfTheRow; i <= middleOfTheRow; i++)
+//     {
+//         for (int j = 0; j < Cols; j++)
+//         {
+//             printf(" %0*d   ", 2, arr[i][j]);
+//         }
+//     }
+// }
+
+// void PrintMiddleColOfMatrix(int arr[3][3], short Row, short Cols)
+// {
+// cout << floor(3 % 2);//1
+// int middleOfTheRow = 1;
+
+//     int middleOfTheCol = floor(Row % 2);//3% 2 = 1
+// arr[0][1]
+// arr[1][1]
+// arr[2][1]
+//     for (int j = 0; j < Cols; j++)
+//     {
+//         for (int i = middleOfTheCol; i <= middleOfTheCol; i++)
+//         {
+//             printf(" %0*d   ", 2, arr[j][i]);
+//         }
+//     }
+// }
 
 int main()
 {
-    return 0;
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+    FillMatrixWithOrderedNumbers(arr, 3, 3);
+
+    cout << "Matrix 1 :\n";
+    PrintMatrix(arr, 3, 3);
+
+    cout << "\nMiddle Row of Matrix 1 is : \n";
+    PrintMiddleRowOfMatrix(arr, 3, 3);
+
+    cout << "\nMiddle Col of Matrix 1 is : \n";
+    PrintMiddleColOfMatrix(arr, 3, 3);
+
+    system("pause>0");
 }
--- Problem-10-Sum-Of-Matrix/Solution-01.cpp
+++ Problem-10-Sum-Of-Matrix/Solution-01.cpp
@@ -4,11 +4,69 @@
  */
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
+
 using namespace std;
 
-// Write a program to fill a 3x3 matrix with random numbers, then write a function to sum all numbers in the matrix and print the result.
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols)
+{
+
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+
+            arr[i][j] = RandomNumber(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols)
+{
+    for (int i = 0; i < Rows; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            // cout << setw(3) << "   ";
+            printf("%0*d  ", 2, arr[i][j]);
+        }
+        cout << endl;
+    }
+}
+
+int SumOfMatrix(int arr[3][3], short Row, short Cols)
+{
+    int Sum = 0;
+    for (int i = 0; i < Row; i++)
+    {
+        for (int j = 0; j < Cols; j++)
+        {
+            Sum +=arr[i][j];
+        }
+    }
+    return Sum;
+}
 
 int main()
 {
-    return 0;
+    srand((unsigned)time(NULL));
+
+    int arr[3][3];
+    FillMatrixWithOrderedNumbers(arr, 3, 3);
+
+    cout << "Matrix 1 :\n";
+    PrintMatrix(arr, 3, 3);
+
+    cout << "Sum of Matrix1 is : ";
+   cout<< SumOfMatrix(arr, 3, 3);
+
+    system("pause>0");
 }
